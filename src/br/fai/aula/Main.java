87a99bc13d203706a0561273cf88ffdd6da5befe
Guilherme <guisrsk8@gmail.com>
2020-03-18 23:47:09 -0300

    primeiro commit

---
+++ src/br/fai/aula/Main.java
@@ -0,0 +1,28 @@
+package br.fai.aula;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+		
+		int opcao = 0;
+		
+		do {
+			System.out.println("1. Cadastrar cavalo");
+			System.out.println("2. Cadastrar girafa");
+			System.out.println("3. Listar cavalos");
+			System.out.println("4. Listar girafas0");
+			System.out.println("5. Listar todos os animais");
+			System.out.println("6. remover umanimal por id");
+			System.out.println("7. Exibir a quantidade de animais cadastrados por tipo");
+			System.out.println("8. Sair");
+			
+		}while(opcao == 8);
+	}
+
+}
